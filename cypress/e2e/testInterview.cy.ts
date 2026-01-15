740059e75e5c4b994294abc2be0873c4efef4072
Marco Vinicio Calderon Alpizar <115949217+mavicaal@users.noreply.github.com>
2026-01-15 17:05:58 -0600

    setting up interview workspace

---
+++ cypress/e2e/testInterview.cy.ts
@@ -0,0 +1,17 @@
+import { expect } from 'chai';
+
+describe('Test Interview Scenarios', () => {
+  // Instantiate page object
+
+  beforeEach(() => {
+    // Preconditions
+  });
+
+  it('TC001:', () => {
+    // Test 1
+  });
+
+  it('TC002:', () => {
+    // Test 2 
+  });
+});
